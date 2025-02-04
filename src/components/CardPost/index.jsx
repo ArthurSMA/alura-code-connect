26c42d1b40e5ee8679a02c3d42d20bb9e81d37db
ArthurSMA <2003Arla2003@>
2025-02-04 17:50:19 -0300

    Server Actions

---
+++ src/components/CardPost/index.jsx
@@ -0,0 +1,43 @@
+import Link from "next/link";
+import Image from "next/image";
+import { Avatar } from "../Avatar";
+import { IconButton } from "../IconButton";
+import { ThumbsUp } from "../icons/ThumbsUp";
+import { incrementThumbsUp } from "@/actions";
+import styles from "./cardpost.module.css";
+
+export const CardPost = ({ post, highlight }) => {
+  const submitThumbsUp = incrementThumbsUp.bind(null, post);
+
+    return (
+    <article className={styles.card} style={{ width: highlight ? 993 : 486 }}>
+      <header className={styles.header}>
+        <figure style={{ height: highlight ? 300 : 133 }}>
+          <Image
+            src={post.cover}
+            fill
+            alt={`Capa do post de titulo: ${post.title}`}
+          />
+        </figure>
+      </header>
+      <section className={styles.body}>
+        <h2>{post.title}</h2>
+        <p>{post.body}</p>
+        <Link href={`/posts/${post.slug}`}>Ver detalhes</Link>
+      </section>
+      <footer className={styles.footer}>
+        <div>
+            <form action={submitThumbsUp}>
+                <IconButton>
+                    <ThumbsUp />
+                </IconButton>
+            </form>
+            <p>
+                {post.likes}
+            </p>
+        </div>
+        <Avatar imageSrc={post.author.avatar} name={post.author.username} />
+      </footer>
+    </article>
+  );
+};
